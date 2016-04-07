87135c20341e630d54adb37f68f3acc864be25e5
Vsevolod Kvachev <rasielll@gmail.com>
2016-04-07 18:24:51 +0300

    Initialized project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by rasiel on 07.04.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
